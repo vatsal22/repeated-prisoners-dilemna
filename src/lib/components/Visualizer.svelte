76d45093b9e829f9173ec14e540b4e960187b42d
Vatsal Solanki <vatsol06@gmail.com>
2026-02-08 15:59:08 -0800

    5x default speed

--- src/lib/components/Visualizer.svelte
+++ src/lib/components/Visualizer.svelte
@@ -10,7 +10,7 @@
 
 	let currentRound = $state(0);
 	let isPlaying = $state(false);
-	let speed = $state(1);
+	let speed = $state(5);
 	let intervalId: ReturnType<typeof setInterval> | null = null;
 
 	// Reset visualizer when match changes
